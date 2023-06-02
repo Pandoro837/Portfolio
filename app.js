20ae8b4f120aa2cc15729e6f202a620b2d9763d7
young <mould837@naver.com>
2023-06-02 22:26:35 +0900

    레이아웃 배치 및 라우터 연결 중

--- app.js
+++ app.js
@@ -1,14 +1,18 @@
 const express = require('express');
+const path = require('path');
 
 const app = express();
-
 app.use(express.static('public'));
+
+// views set
+app.set('views', path.join(__dirname, './views'));
+app.set("view engine", "ejs");
 
 const port = 3000;
 
 // main
 app.use('/', (req, res) => {
-
+    res.render('index');
 });
 
 app.use('/cms', (req, res) =>{
