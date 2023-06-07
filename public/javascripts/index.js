0950b51f8d94b6b18916fbd50c5876a8b784e65c
young <mould837@naver.com>
2023-06-07 23:11:51 +0900

    front 레이아웃 완료, modal 구현

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -10,10 +10,33 @@ language.addEventListener('click', () => {
 /** 입력된 국가로 언어 번경 */
 function change_language(country) {
     console.log(country);
-}
+};
 
+/* modal */
+const modals = document.getElementsByClassName('modal');
 const login = document.getElementById('login');
+let thumb = document.getElementById('project-thumb');
 
+/** login modal click */
 login.addEventListener('click', () => {
-    console.log('login');
+    let login_modal = document.getElementById('modal-login');
+    modal_toggle(login_modal);
+});
+
+thumb.addEventListener('click', () => {
+    let project_modal = document.getElementById('modal-project');
+    modal_toggle(project_modal);
+});
+
+/** Modal 열기 */
+function modal_toggle(modal) {
+    modal.classList.toggle('active');
+    //document.body.classList.toggle('modal-open')
+};
+
+/** modal 닫기 */
+window.addEventListener('click', (e) => {
+    if(e.target.classList.contains('modal')) {
+        modal_toggle(e.target);
+    }
 });
