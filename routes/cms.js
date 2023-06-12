ac6c81f3f30ce3a64e6714176e4d103e9b8f5516
young <mould837@naver.com>
2023-06-12 23:40:48 +0900

    ejs 변수 전달

--- routes/cms.js
+++ routes/cms.js
@@ -2,7 +2,12 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/', (req, res) => {
-    res.render('./cms/cms')
+    res.render('./cms/cms');
+});
+
+router.get('/:type', (req, res) => {
+    const {type} = req.params;
+    res.render('./cms/cms', {type : type});
 });
 
 module.exports = router;
