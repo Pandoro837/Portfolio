3ca756e14550bae28a5dd77f0a7f0b1327cb89d7
young <mould837@naver.com>
2023-06-22 23:21:28 +0900

    router set

--- routes/cms.js
+++ routes/cms.js
@@ -1,9 +1,9 @@
 const express = require('express');
 const router = express.Router();
 
-// router.get('/', (req, res) => {
-//     res.render('./cms/cms');
-// });
+router.get('/', (req, res) => {
+    res.render('./cms/cms', {type : 'introduce'});
+});
 
 router.get('/:type', (req, res) => {
     const {type} = req.params;
